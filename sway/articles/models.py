b288b8df6b9f81505b765a70c6735e592233c143
Milo Bedini <milobedini64@gmail.com>
2021-12-01 18:10:32 +0000

    articles added

--- sway/articles/models.py
+++ sway/articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    views = models.IntegerField(default=0)
+    text = models.TextField()
+
+    def __str__(self):
+        return "Article: " + self.title
--- sway/articles/views.py
+++ sway/articles/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Article
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
+
+from articles.serializers.ArticleSerializer import ArticleSerializer
 
 # Create your views here.
+
+
+class ArticleListView(APIView):
+    # permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serialized_articles = ArticleSerializer(
+            articles, many=True)
+        return Response(serialized_articles.data, status=status.HTTP_200_OK)
+
+
+class ArticleDetailView(APIView):
+    # permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get(self, request, pk):
+        article = Article.objects.get(id=pk)
+        serialized_article = ArticleSerializer(article)
+        return Response(serialized_article.data, status=status.HTTP_200_OK)
