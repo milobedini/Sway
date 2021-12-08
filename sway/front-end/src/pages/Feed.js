64a5c57232ed9a8be55cec90d862a6ee4575121b
Milo Bedini <milobedini64@gmail.com>
2021-12-08 17:24:55 +0000

    filter feed

--- sway/front-end/src/pages/Feed.js
+++ sway/front-end/src/pages/Feed.js
@@ -3,9 +3,19 @@ import axios from 'axios'
 import ArticleCard from '../components/ArticleCard'
 import '../styles/feed.scss'
 import { Link } from 'react-router-dom'
+import { FcPlus } from 'react-icons/fc'
+import FilterButton from '../components/FilterButton'
+
+const filterMap = {
+  All: () => true,
+  Article: (post) => post.category.includes('Article'),
+  Thread: (post) => post.category.includes('Thread'),
+}
+const filterNames = Object.keys(filterMap)
 
 const Feed = () => {
   const [feed, setFeed] = useState([])
+  const [filter, setFilter] = useState('All')
 
   useEffect(() => {
     const getFeed = async () => {
@@ -15,18 +25,44 @@ const Feed = () => {
     }
     getFeed()
   }, [])
+
+  const filterList = filterNames.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ))
+
+  const postList = feed
+    .filter(filterMap[filter])
+    .map((post) => (
+      <li key={post.id}>
+        {<ArticleCard createdAt={post.created_at} {...post} />}
+      </li>
+    ))
+
   return (
     <div className="feed">
-      <Link to="/feed/new/">
-        <button className="btn">Start Thread</button>
-      </Link>
-      <ul>
-        {feed.map((article) => (
-          <li key={article.id}>
-            {<ArticleCard createdAt={article.created_at} {...article} />}
-          </li>
-        ))}
-      </ul>
+      <div className="feed-heading">
+        <h2>Feed</h2>
+        <p>Check out our community forum and much more.</p>
+      </div>
+      <div className="start-thread">
+        <Link to="/feed/new/">
+          <button className="btn">
+            <FcPlus size="2.2rem" />
+          </button>
+        </Link>
+      </div>
+      <div className="categories">
+        <h5>Choose a Category:</h5>
+        <ul>{filterList}</ul>
+      </div>
+      <div className="post-list">
+        <ul>{postList}</ul>
+      </div>
     </div>
   )
 }
